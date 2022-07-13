c13d45b045c9194d0846f492652722f54093d35f
NovusEdge <novusedge0@gmail.com>
2022-07-14 04:13:52 +0430

    Adding solution to problem 1 in python

---
+++ python-solutions/problem001.py
@@ -0,0 +1,10 @@
+from time import time
+
+def solution():
+    return sum(filter(lambda x: x%3 == 0 or x%5 == 0, range(0, 1000)))
+
+if __name__ == "__main__":
+    start  = time()
+    answer = solution()
+    print(f"\nAnswer to Problem 1: {answer}")
+    print(f"Time Taken         : {time()-start} seconds\n")
