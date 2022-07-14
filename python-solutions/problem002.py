fef2275b2821439fb818099e7a39a8f2d657524d
NovusEdge <novusedge0@gmail.com>
2022-07-14 05:19:57 +0430

    Adding python solution to problem: 2

---
+++ python-solutions/problem002.py
@@ -0,0 +1,24 @@
+from time import time
+from math import sqrt
+
+def F(n):
+    return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
+
+def solution():
+    i, sum = 1, 0
+    while True:
+        t = int(F(i))
+        if t > 4_000_000: break
+        if t%2 == 0: 
+            sum += t
+        i += 1
+        
+    return sum
+
+
+
+if __name__ == "__main__":
+    start  = time()
+    answer = solution()
+    print(f"\nAnswer to Problem 2: {answer}")
+    print(f"Time Taken         : {time()-start} seconds\n")
